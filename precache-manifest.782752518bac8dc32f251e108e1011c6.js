63c10dad6139c9fe58e87e8723668b3f4f0cbb9d
Kaija Dunklin <kaykay_s@yahoo.com>
2020-08-11 13:47:06 -0400

    Updates

---
+++ precache-manifest.782752518bac8dc32f251e108e1011c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd67865bf5e5655cf9bc763c49d04344",
+    "url": "/kaidunklin23/Todo-List.git/index.html"
+  },
+  {
+    "revision": "63d13c07dac73fb53a94",
+    "url": "/kaidunklin23/Todo-List.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "735e3a42a953643d6403",
+    "url": "/kaidunklin23/Todo-List.git/static/js/2.12e6d84c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kaidunklin23/Todo-List.git/static/js/2.12e6d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d13c07dac73fb53a94",
+    "url": "/kaidunklin23/Todo-List.git/static/js/main.d1432574.chunk.js"
+  },
+  {
+    "revision": "b1a10ee4db5f71c488a0",
+    "url": "/kaidunklin23/Todo-List.git/static/js/runtime-main.d97739f2.js"
+  }
+]);
